7a91e3174380aaa089f5afbd333802809b5f6976
koechemi <koechemi@E211-23-002>
2026-04-14 14:04:47 +0200

    Ich habe beide

--- VTM-Client-AIF22/IModel.cs
+++ VTM-Client-AIF22/IModel.cs
@@ -7,9 +7,9 @@ namespace VTM_Client_AIF22
         IView View { set; }
         IController Controller { set; }
 
-        void DatenSenden(bool out1, bool out2);
+        void DatenSenden(bool out1, bool out2, bool out3, bool out4,);
 
-        void DatenEmpfangen(bool in1, bool in2);
+        void DatenEmpfangen(bool in1, bool in2, bool in3, bool in4);
 
     }
 
